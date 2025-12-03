8ced8fb33c401ede45859dd79f0dd53b79361857
Margo Boldar <mikhailmargo@yandex.ru>
2025-12-03 15:18:25 +0300

    Внести правки

--- infra/config-server/src/main/java/ru/yandex/practicum/ConfigServer.java
+++ infra/config-server/src/main/java/ru/yandex/practicum/ConfigServer.java
@@ -1,3 +1,5 @@
+package ru.yandex.practicum;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -5,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServer {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
     }
